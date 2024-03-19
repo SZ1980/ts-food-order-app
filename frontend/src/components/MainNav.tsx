ade5855f831f7c6d99ef1ab4e264087529e36f6b
Zoran Simonovic <simonovic1980@gmail.com>
2024-03-19 14:07:21 +0100

    auth0 sign up user with google

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,14 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
-import UsernameMenu from "./UsernameMenu";
-import { Link } from "react-router-dom";
+// import UsernameMenu from "./UsernameMenu";
+// import { Link } from "react-router-dom";
+// , isAuthenticated
 
 const MainNav = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
     <span className="flex space-x-2 items-center">
-      {isAuthenticated ? (
+      {/* {isAuthenticated ? (
         <>
           <Link to="/order-status" className="font-bold hover:text-orange-500">
             Order Status
@@ -16,14 +17,15 @@ const MainNav = () => {
           <UsernameMenu />
         </>
       ) : (
-        <Button
-          variant="ghost"
-          className="font-bold hover:text-orange-500 hover:bg-white"
-          onClick={async () => await loginWithRedirect()}
-        >
-          Log In
-        </Button>
-      )}
+       
+      )} */}
+      <Button
+        variant="ghost"
+        className="font-bold hover:text-orange-500 hover:bg-white"
+        onClick={async () => await loginWithRedirect()}
+      >
+        Log In
+      </Button>
     </span>
   );
 };
